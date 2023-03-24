ca1410365aafff61bf68c6dad4d5aab0cdab33b0
Olivier Liess <olivier.liess@eurodecision.com>
2023-03-24 22:53:27 +0100

    updated generate.sh script with diffrent settings

--- generate.sh
+++ generate.sh
@@ -1,43 +1,71 @@
 #!/bin/bash
 
-rm -rf builds src
-mkdir -p src
+init () {
+	rm -rf builds src
+	mkdir -p src
 
-for i in `seq -w 00 09`
-do
-	cat >src/fn_$i.cc <<-EOF
-		#include <iostream>
-		void fn_$i() {
-		   std::cout << "$i" << std::endl;
+	for i in `seq -w 00 09`
+	do
+		cat >src/fn_$i.cc <<-EOF
+			#include <iostream>
+			void fn_$i() {
+			   std::cout << "$i" << std::endl;
+			}
+		EOF
+	done
+
+	cat >>src/main.cc <<-EOF
+	int main () {
+	EOF
+	for i in `seq -w 00 04`
+	do
+		cat >>src/main.cc <<-EOF
+			   extern void fn_$i ();
+			   fn_$i ();
+		EOF
+	done
+	cat >>src/main.cc <<-EOF
 		}
 	EOF
-done
+}
 
-cat >>src/main.cc <<EOF
-int main () {
-EOF
-for i in `seq -w 00 04`
-do
-	cat >>src/main.cc <<-EOF
-		   extern void fn_$i ();
-		   fn_$i ();
-	EOF
-done
-cat >>src/main.cc <<-EOF
-	}
-EOF
+build () {
 
-for i in a b c
-do
-	cmake -S . -B builds/$i
-	cmake --build builds/$i
-done
+	for i in a b c
+	do
+		cmake -S . -B builds/$i
+		cmake --build builds/$i
+	done
+}
 
-for i in a b c
-do
-	mkdir -p builds/$i/coverage-reports/gcovr
-	gcovr --gcov-executable /usr/bin/gcov \
-		--root $PWD \
-		--xml --xml-pretty --output $PWD/builds/$i/coverage-reports/gcov-results.xml \
-		builds/$i &
-done
+generate_build () {
+	SRC_DIR=$PWD
+	for i in a b c
+	do
+		mkdir -p builds/$i/coverage-reports/gcovr
+		cd builds/$i
+		gcovr --gcov-executable /usr/bin/gcov \
+			--root $SRC_DIR \
+			--xml --xml-pretty --output coverage-reports/gcov-results.xml &
+		cd -
+	done
+}
+
+generate_root () {
+	SRC_DIR=$PWD
+	for i in a b c
+	do
+		mkdir -p builds/$i/coverage-reports/gcovr
+		gcovr --gcov-executable /usr/bin/gcov \
+			--root $SRC_DIR \
+			--xml --xml-pretty --output builds/$i/coverage-reports/gcov-results.xml \
+			builds/$i &
+		cd -
+	done
+}
+
+init
+build
+#generate_build
+generate_root
+
